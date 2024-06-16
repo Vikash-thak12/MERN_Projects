72feca01abddeeca6ee3487050dac5fc2fa334d3
Vikash Thakur <thakvikash622@gmail.com>
2024-06-16 21:27:32 +0530

    Basic change

--- BookStore/FrontEnd/src/components/Footer.jsx
+++ BookStore/FrontEnd/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
                     </div>
                 </nav>
                 <aside>
-                    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2024 - All right reserved by Vikash Thakur.</p>
                 </aside>
             </footer>
         </>
