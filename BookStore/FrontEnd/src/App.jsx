6fae132e2bff6a2ab12d22ce7e3800fc67d04e25
Vikash Thakur <thakvikash622@gmail.com>
2024-06-16 14:09:24 +0530

    Created a separate folder for better optimization

--- BookStore/FrontEnd/src/App.jsx
+++ BookStore/FrontEnd/src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import FreeBook from './components/FreeBook'
-import Navbar from './components/Navbar'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Banner />
-    <FreeBook />
-    <Footer /> 
+    <Home />
     </>
   )
 }
