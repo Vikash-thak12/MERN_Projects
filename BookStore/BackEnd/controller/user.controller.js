5a21de2e6dcf661002ac51f5672e0880c7b41ae9
Vikash Thakur <thakvikash622@gmail.com>
2024-06-18 00:02:58 +0530

    login functionaly is working

--- BookStore/BackEnd/controller/user.controller.js
+++ BookStore/BackEnd/controller/user.controller.js
@@ -4,36 +4,46 @@ import bcryptjs from 'bcryptjs'
 export const signup = async (req, res) => {
     try {
         // reveiving the body from from the user
-        const {fullname, email, password} = req.body;
+        const { fullname, email, password } = req.body;
         const user = await User.findOne({ email })  // Finding those particular user 
-        if(user) {
-            return res.status(400).json({ Message: "User Already exist"})
+        if (user) {
+            return res.status(400).json({ Message: "User Already exist" })
         }
 
         const hashPassword = await bcryptjs.hash(password, 10)  // for securing the password, use bcryptjs for hashing the password
         // If user is not there in the database then creating new one 
         const createUser = new User({
-            fullname: fullname, 
+            fullname: fullname,
             email: email,
             password: hashPassword
         })
         await createUser.save();
-        res.status(201).json({Message: "User created Successfully"})
+        res.status(201).json({ Message: "User created Successfully" })
     } catch (error) {
         console.log("Error :", error);
-        res.status(500).json({ Message: "Internal server Error"})
+        res.status(500).json({ Message: "Internal server Error" })
     }
 }
 
 export const login = async (req, res) => {
     try {
-        const {email, password} = req.body;
-        const user = await User.findOne({email})
+        const { email, password } = req.body;
+        const user = await User.findOne({ email })
         const isMatch = await bcryptjs.compare(password, user.password)
-        if(!user || !isMatch) {
-            return res.status(400).json({ Message: "Invalid username and password"})
+        if (!user || !isMatch) {
+            return res.status(400).json({ Message: "Invalid username and password" })
+        } else {
+            return res.status(200).json({
+                Message: "Login Successful",
+                user: {
+                    _id: user._id,
+                    fullname: user.fullname,
+                    email: user.email
+                }
+            }
+            )
         }
     } catch (error) {
-        
+        console.log("Error:", error)
     }
 }
