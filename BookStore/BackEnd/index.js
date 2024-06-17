e7951e99e4dc61abbfe716bddd274323a3427f52
Vikash Thakur <thakvikash622@gmail.com>
2024-06-17 22:20:41 +0530

    Successfully connect the mongoDB

--- BookStore/BackEnd/index.js
+++ BookStore/BackEnd/index.js
@@ -2,16 +2,33 @@
 // const dotenv = require('dotenv')
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8000;
+const URL = process.env.MongoDBUrl;
 
-app.get('/', (req, res) => {
-  res.send('This is vikash')
-})
+
+// Connection to the mongoDb
+try {
+  mongoose.connect(URL)
+  console.log("MongoDB Connected:");
+} catch (error) {
+  console.log("Error: ", error);
+}
+try {
+  mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  console.log("MongoDB Connected:");
+} catch (error) {
+  console.log("Error: ", error);
+}
+
 
 app.listen(PORT, () => {
   console.log(`The server is running on PORT: ${PORT}`)
