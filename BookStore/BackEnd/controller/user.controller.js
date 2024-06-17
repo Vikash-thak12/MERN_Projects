0ca0173534b3d3d70fa6b62081b6bcf3159ec1bf
Vikash Thakur <thakvikash622@gmail.com>
2024-06-17 23:41:05 +0530

    Necessary comments

--- BookStore/BackEnd/controller/user.controller.js
+++ BookStore/BackEnd/controller/user.controller.js
@@ -2,11 +2,14 @@ import User from "../model/user.model.js";
 
 export const signup = async (req, res) => {
     try {
+        // reveiving the body from from the user
         const {fullname, email, password} = req.body;
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email })  // Finding those particular user 
         if(user) {
             return res.status(400).json({ Message: "User Already exist"})
         }
+
+        // If user is not there in the database then creating new one 
         const createUser = new User({
             fullname, 
             email,
