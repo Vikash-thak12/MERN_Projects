548ac8da8159c1ee098ae29851dab4f9cf4e36ff
Vikash Thakur <thakvikash622@gmail.com>
2024-06-19 18:12:56 +0530

    Created a logout button

---
+++ BookStore/FrontEnd/src/components/Logout.jsx
@@ -0,0 +1,11 @@
+
+
+const Logout = () => {
+    return (
+        <div className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>
+            Logout
+        </div>
+    );
+};
+
+export default Logout;
