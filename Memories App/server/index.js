4cd36b5798ad4c0e693c97a2910e37c455a50bdf
Vikash Thakur <thakvikash622@gmail.com>
2024-06-25 13:51:43 +0530

    Basic changes

--- Memories App/server/index.js
+++ Memories App/server/index.js
@@ -2,3 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors'
 import bodyParser from 'body-parser';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
