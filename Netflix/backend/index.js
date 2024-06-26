024d37e0a5b85271d1a68d43a4c76ae801c16d8b
Vikash Thakur <thakvikash622@gmail.com>
2024-06-26 08:52:13 +0530

    connection of mongodb and and middlewares

--- Netflix/backend/index.js
+++ Netflix/backend/index.js
@@ -1,9 +1,30 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 const app = express()
 dotenv.config();
+
+// files importing from .env
 const PORT = process.env.PORT || 8000
+const Url = process.env.Mongo_Url;
+
+
+// middlrewares 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(cookieParser)
+
+
+
+// Connection to the mongoDB
+try {
+    mongoose.connect(Url)
+    console.log("MongoDB connected");
+} catch (error) {
+    console.log("Error", error);
+}
 
 
 app.listen(PORT, () => console.log(`Server stated on PORT: ${PORT}`))
