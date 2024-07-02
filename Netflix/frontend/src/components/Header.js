c4d9acbdeb08244a4631de9c41ee85111c64b368
Vikash Thakur <thakvikash622@gmail.com>
2024-07-02 13:53:27 +0530

    Protected the browse route

--- Netflix/frontend/src/components/Header.js
+++ Netflix/frontend/src/components/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { IoIosArrowDropdown } from "react-icons/io";
 import netflix from '../assets/netflix.svg'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const user = useSelector((store) => store.app.user)
+  const navigate = useNavigate();
   // console.log(user);
+  useEffect(() => {
+    if(!user){
+      navigate("/")
+    }
+  },[])
   return (
     <div className='flex justify-between px-5 py-3  bg-gradient-to-b from-black'>
       <div>
@@ -17,7 +24,7 @@ const Header = () => {
             <>
               <div className='flex items-center gap-1 text-white'>
                 <IoIosArrowDropdown size={24} />
-                <h1 className='font-medium'>Vikash Thakur</h1>
+                <h1 className='font-medium'>{user.fullname}</h1>
               </div>
               <button className='bg-red-600 p-1 rounded-lg px-3 text-white'>Logout</button>
               <button className='bg-red-600 p-1 rounded-lg px-3 text-white'>Search Movie</button>
