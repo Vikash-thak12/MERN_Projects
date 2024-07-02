d6b6b11206189e535503e5cf0db54055a7aca2c3
Vikash Thakur <thakvikash622@gmail.com>
2024-07-02 20:40:33 +0530

    Basic changes

--- Netflix/frontend/src/components/Browse.js
+++ Netflix/frontend/src/components/Browse.js
@@ -4,28 +4,16 @@ import MovieContainer from './MovieContainer.js'
 import MainContainer from './MainContainer.js'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import axios from 'axios'
-import { Now_Playing_API, options } from '../utils/contant.js'
 
 const Browse = () => {
   const user = useSelector((store) => store.app.user)
   const navigate = useNavigate();
 
 
-  const nowPlayingMovies = async () => {
-    try {
-      const res = await axios.get(Now_Playing_API, options)
-      console.log(res.data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   useEffect(() => {
     if(!user){
       navigate("/")
     }
-    nowPlayingMovies();
   },[])
 
 
--- Netflix/frontend/src/redux/movieSlice.js
+++ Netflix/frontend/src/redux/movieSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const nowPlayingMovies = createSlice({
-    name: "movies",
+    name: "movie",
     initialState: {
         nowPlayingMovies: null
     },
