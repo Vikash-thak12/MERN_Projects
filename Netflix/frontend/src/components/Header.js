822fbe455d3b408a75ad8db0ff59d348b700c246
Vikash Thakur <thakvikash622@gmail.com>
2024-07-02 14:03:20 +0530

    Successfully implemented logout functionality

--- Netflix/frontend/src/components/Header.js
+++ Netflix/frontend/src/components/Header.js
@@ -1,18 +1,41 @@
 import React, { useEffect } from 'react'
 import { IoIosArrowDropdown } from "react-icons/io";
 import netflix from '../assets/netflix.svg'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { API_END_POINT } from '../utils/contant';
+import { setUser } from '../redux/userSlice';
+import toast from 'react-hot-toast';
 
 const Header = () => {
   const user = useSelector((store) => store.app.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   // console.log(user);
   useEffect(() => {
     if(!user){
       navigate("/")
     }
   },[])
+
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${API_END_POINT}/logout`)
+      console.log(res);
+      if(res.data.success) {
+        toast.success(res.data.Message)
+      }
+      dispatch(setUser(null))
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
   return (
     <div className='flex justify-between px-5 py-3  bg-gradient-to-b from-black'>
       <div>
@@ -26,7 +49,7 @@ const Header = () => {
                 <IoIosArrowDropdown size={24} />
                 <h1 className='font-medium'>{user.fullname}</h1>
               </div>
-              <button className='bg-red-600 p-1 rounded-lg px-3 text-white'>Logout</button>
+              <button onClick={logoutHandler} className='bg-red-600 p-1 rounded-lg px-3 text-white'>Logout</button>
               <button className='bg-red-600 p-1 rounded-lg px-3 text-white'>Search Movie</button>
             </>
           )
