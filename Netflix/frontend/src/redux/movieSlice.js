c4f056914329d7f8057a5d2348fea89229bd3f27
Vikash Thakur <thakvikash622@gmail.com>
2024-07-03 09:11:16 +0530

    made necessary slice for the movies

--- Netflix/frontend/src/redux/movieSlice.js
+++ Netflix/frontend/src/redux/movieSlice.js
@@ -5,16 +5,24 @@ const nowPlayingMovies = createSlice({
     initialState: {
         nowPlayingMovies: null,
         popularMovies: null,
+        topRatedMovies: null,
+        upcomingMovies: null,
     },
     reducers: {
         getNowPlayingMovies: (state, action) => {
             state.nowPlayingMovies = action.payload;
         },
         getPopularMovies: (state, action) => {
-            state.popularMovie = action.payload
+            state.popularMovies = action.payload
+        },
+        getTopRatedMovies: (state, action) => {
+            state.topRatedMovies = action.payload
+        },
+        getUpcomingMovies: (state, action) => {
+            state.upcomingMovies = action.payload
         }
     }
 })
 
-export const {getNowPlayingMovies, getPopularMovies} = nowPlayingMovies.actions;
+export const {getNowPlayingMovies, getPopularMovies, getTopRatedMovies, getUpcomingMovies} = nowPlayingMovies.actions;
 export default nowPlayingMovies.reducer;
