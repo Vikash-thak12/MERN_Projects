9d4ed647530b3a9b13a04e2a7d4ada21be9d765f
Vikash Thakur <thakvikash622@gmail.com>
2024-07-04 21:41:33 +0530

    Working on search functionality

--- Netflix/frontend/src/components/SearchMovies.js
+++ Netflix/frontend/src/components/SearchMovies.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchMovies = () => {
+
+  const [searchMovie, setSearchMovie] = useState("")
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className='flex justify-center pt-[10%] w-[100%]'>
-      <form action="submit" className='border-2 rounded-lg p-2 w-[50%] flex justify-between gap-2'>
-        <input className='outline-none w-full' type="text" name="search" placeholder="Search Movies" />
+      <form onSubmit={submitHandler} action="submit" className='border-2 rounded-lg p-2 w-[50%] flex justify-between gap-2'>
+        <input value={searchMovie} onChange={(e) => {setSearchMovie(e.target.value)}} className='outline-none w-full' type="text" name="search" placeholder="Search Movies" />
         <button className='bg-red-800 hover:bg-red-700 px-2 py-1 rounded-md text-white'>Search</button>
       </form>
     </div>
