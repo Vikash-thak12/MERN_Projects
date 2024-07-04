2549b46490a8a20ba5486935e280cc9d213afa53
Vikash Thakur <thakvikash622@gmail.com>
2024-07-04 22:19:45 +0530

    Basic changes

--- Netflix/frontend/src/components/Movielist.js
+++ Netflix/frontend/src/components/Movielist.js
@@ -4,14 +4,14 @@ import MovieCard from './MovieCard'
 const Movielist = ({ title, movie = [] }) => {
     // console.log({ movie });
       // Ensure movie is an array before mapping
-      const moviesToRender = Array.isArray(movie) ? movie : [];
+    //   const moviesToRender = Array.isArray(movie) ? movie : [];
     return (
         <div className='z-10'>
             <h1 className='text-3xl font-bold text-white px-2 py-1'>{title}</h1>
             <div className='flex overflow-x-auto no-scrollbar'>
                 <div className='flex items-center gap-2 px-2 py-2'>
                     {
-                        moviesToRender.map((item) => {
+                        movie && movie.map((item) => {
                             return (
                                 <MovieCard key={item.id} item={item} posterpath={item.poster_path} />
                             )
--- Netflix/frontend/src/components/SearchMovies.js
+++ Netflix/frontend/src/components/SearchMovies.js
@@ -2,32 +2,50 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { options, SEARCH_MOVIE_API } from '../utils/contant';
 import Movielist from './Movielist';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchMoviesDetails } from '../redux/searchSlice';
+import { setLoading } from '../redux/userSlice';
 
 const SearchMovies = () => {
 
   const [searchMovie, setSearchMovie] = useState("")
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(store => store.app.isLoading)
+
+  // handler function 
   const submitHandler = async (e) => {
     e.preventDefault();
+    dispatch(setLoading(true))
     try {
       const res = await axios.get(`${SEARCH_MOVIE_API}${searchMovie}&include_adult=false&language=en-US&page=1`, options)
       console.log(res.data.results);
+      const movies = res.data.results;
+      dispatch(setSearchMoviesDetails({ searchMovie, movies }))
     } catch (error) {
       console.log(error);
+    } finally {
+      dispatch(setLoading(false))
     }
+    setSearchMovie("")
   }
 
+  const {movieName, searchedMovies} = useSelector(store => store.search)
+
   return (
-    <>
+    <div className='bg-gray-500 h-screen'>
       <div className='flex justify-center pt-[10%] w-[100%]'>
         <form onSubmit={submitHandler} action="submit" className='border-2 rounded-lg p-2 w-[50%] flex justify-between gap-2'>
-          <input value={searchMovie} onChange={(e) => { setSearchMovie(e.target.value) }} className='outline-none w-full' type="text" name="search" placeholder="Search Movies" />
-          <button className='bg-red-800 hover:bg-red-700 px-2 py-1 rounded-md text-white'>Search</button>
+          <input value={searchMovie} onChange={(e) => { setSearchMovie(e.target.value) }} className='bg-transparent outline-none w-full' type="text" name="search" placeholder="Search Movies" />
+          <button className='bg-red-800 hover:bg-red-700 px-2 py-1 rounded-md text-white'>{isLoading ? "Loading..." : "Search"}</button>
+
         </form>
       </div>
-      <div>
-      {/* <Movielist title={searchMovie} movie={data.popularMovies} /> */}
+      <div className='bg-transparent'>
+        {/* <h1 className='bg-red-500'>{searchMovie}</h1> */}
+        <Movielist title={movieName} movie={searchedMovies} />
       </div>
-    </>
+    </div>
   )
 }
 
