8d16186a677bc71c91c7b4a89380e0e9a54d55a2
Vikash Thakur <thakvikash622@gmail.com>
2024-07-08 15:17:59 +0530

    Created necessary folders

--- ChatApp/frontend/src/App.jsx
+++ ChatApp/frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { lazy } from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 const App = () => {
+  const Home = lazy(() => import("./pages/Home"))
+  const Chat = lazy(() => import("./pages/Chat"))
+  const Login = lazy(() => import("./pages/Login"))
+  
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
     </BrowserRouter>
   );
