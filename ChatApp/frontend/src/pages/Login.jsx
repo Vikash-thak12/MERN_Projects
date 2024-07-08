392f3e921dbb3a7cc9c8fb03a966bbf617d664eb
Vikash Thakur <thakvikash622@gmail.com>
2024-07-08 15:30:12 +0530

    Creating login page

--- ChatApp/frontend/src/pages/Login.jsx
+++ ChatApp/frontend/src/pages/Login.jsx
@@ -3,7 +3,24 @@
 const Login = () => {
   return (
     <div>
-    Login
+      <div>
+        <form action="submit">
+          <div>
+          <label>Enter your Gmail</label>
+          <input type="text" placeholder="Enter your gmail" />
+          </div>
+          <div>
+          <label>Enter your Name</label>
+          <input type="text" placeholder="Enter your Name" />
+          </div>
+
+          <div>
+          <label>Enter your Password</label>
+          <input type="password" placeholder="Enter your Password" />
+          </div>
+
+        </form>
+      </div>
     </div>
   );
 };
