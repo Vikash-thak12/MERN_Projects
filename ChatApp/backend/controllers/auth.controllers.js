3879385edb9ffa11c08bc2433d9fb7d825f917a4
Vikash Thakur <thakvikash622@gmail.com>
2024-07-09 17:59:19 +0530

    Update the code

--- ChatApp/backend/controllers/auth.controllers.js
+++ ChatApp/backend/controllers/auth.controllers.js
@@ -2,19 +2,19 @@ import userModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 export const signup = async (req, res) => {
     try {
-        const {fullname, gmail, password, confirmpassword, gender, profilepic} = req.body;
-        if(password !== confirmpassword) {
-            return res.status(400).json({ Error: "The passwords didn't match"})
+        const { fullname, gmail, password, confirmpassword, gender } = req.body;
+        if (password !== confirmpassword) {
+            return res.status(400).json({ Error: "The passwords didn't match" })
         }
 
-        if(!fullname || !gmail || !password || !gender || !profilepic) {
+        if (!fullname || !gmail || !password || !gender ) {
             return res.status(401).json({
                 Error: "Please fill all the fields"
             })
         }
 
-        const user = await userModel.findOne({gmail})
-        if(user) {
+        const user = await userModel.findOne({ gmail })
+        if (user) {
             return res.status(401).json({ Message: "User already exists" })
         }
 
@@ -22,7 +22,7 @@ export const signup = async (req, res) => {
         const girlprofile = `https://avatar.iran.liara.run/public/girl?username={gmail}`
 
 
-        const hashpassword = await bcryptjs.hash(password, 8) 
+        const hashpassword = await bcryptjs.hash(password, 8)
         const newUser = await userModel.create({
             fullname,
             gmail,
@@ -31,16 +31,21 @@ export const signup = async (req, res) => {
             profilepic: gender === "male" ? boyprofile : girlprofile
         })
 
-        return res.status(200).json({
-            message: "User created successfully",
-            userDetails: {
-                _id: newUser._id,
-                fullname: newUser.fullname,
-                gmail: newUser.gmail,
-                gender: newUser.gender,
+        if (newUser) {
+            return res.status(200).json({
+                message: "User created successfully",
+                userDetails: {
+                    _id: newUser._id,
+                    fullname: newUser.fullname,
+                    gmail: newUser.gmail,
+                    gender: newUser.gender,
+                    profilepic: newUser.profilepic
+                }
             }
-        },
-    )
+            )
+        } else {
+            return res.status(400).json({ Error: "Invalid user Data"})
+        }
 
     } catch (error) {
         console.log("Error in signup Controller", error.message);
