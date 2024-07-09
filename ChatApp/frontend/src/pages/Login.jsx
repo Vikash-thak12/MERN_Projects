9eaef092a472f91d05cf7bbf6579a91c8e18a02a
Vikash Thakur <thakvikash622@gmail.com>
2024-07-09 12:50:29 +0530

    Done with login and signup

--- ChatApp/frontend/src/pages/Login.jsx
+++ ChatApp/frontend/src/pages/Login.jsx
@@ -2,50 +2,109 @@
 // import bg from "../assets/loginbg.jpg"
 import { useState } from "react";
 import bg from "../assets/loginbg1.jpg"
-import { Avatar, IconButton, Stack } from "@mui/material";
+import { Avatar, IconButton, Stack, Typography } from "@mui/material";
 import { CameraAlt } from "@mui/icons-material";
 import { VisuallyHiddenInput } from "../components/styles/StyledComponent";
+import { useFileHandler, useInputValidation } from '6pp'
 
 
 const Login = () => {
-  const [islogin, setIslogin] = useState(true)
+  const [islogin, setIslogin] = useState(false)
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [password, setPassword] = useState("")
+  const name = useInputValidation("")
+  const email = useInputValidation("")
+  const password = useInputValidation("")
+  const avatar = useFileHandler("single")
+
   const toggle = () => {
     setIslogin(islogin => !islogin)
   }
+
+  const prevent = (e) => {
+    e.preventDefault()
+  }
+
+  
+  
+  // const getInputData = (e) => {
+  //   e.preventDefault();
+  //   //login
+  //   if(islogin) {
+  //     const user = {email, password}
+  //     console.log(user);
+  //   } else {
+  //     const user = {name, email, password}
+  //     console.log(user);
+  //   }
+  //   setName("")
+  //   setEmail("")   
+  //   setPassword("")
+  // }
+
+
   return (
     <div style={{ backgroundImage: `url(${bg})` }} className="bg-cover bg-center bg-no-repeat flex justify-center items-center h-screen">
-      <div className="border p-5 w-1/4 rounded-lg">
+      <div style={{ backdropFilter: "blur(15px)" }} className="border p-5 w-1/4 rounded-lg">
         <h1 className="text-center text-3xl font-bold text-white">{islogin ? "Login" : "SignUp"}</h1>
         <Stack className="relative flex items-center justify-center w-full h-32">
-          <Avatar component="label" style={{ width: '100px', height: '100px', objectFit: "contain", cursor: "pointer"}}>
-          </Avatar>
+          <Avatar component="label" style={{ width: '100px', height: '100px', objectFit: "contain", cursor: "pointer"}} src={avatar.preview} />
             <IconButton component="label" style={{ position: "absolute", bottom: "0", right: "125px", color: "white"}} >
               <>
               <CameraAlt  />
-              <VisuallyHiddenInput type="file" />
+              <VisuallyHiddenInput type="file" onChange={avatar.changeHandler} />
               </>
             </IconButton>
         </Stack>
-        <form className="flex flex-col gap-6">
+        <form onSubmit={prevent} action="submit" className="flex flex-col gap-6">
 
           {
             islogin ? "" : (
               <>
                 <div className="flex flex-col">
                   <label className="font-medium text-xl text-white">Enter your Name</label>
-                  <input className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="text" placeholder="Enter your Name" />
+                  <input value={name.value} onChange={name.changeHandler} className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="text" placeholder="Enter your Name" />
                 </div>
               </>
             )
           }
           <div className="flex flex-col">
-            <label className="font-medium text-xl text-white">Enter your Gmail</label>
-            <input className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="text" placeholder="Enter your gmail" />
+            <label className="font-medium text-xl text-white">Enter your Email</label>
+            <input value={email.value} onChange={email.changeHandler}  className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="email" placeholder="Enter your email" />
           </div>
 
           <div className="flex flex-col">
             <label className="font-medium text-xl text-white">Enter your Password</label>
-            <input className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="password" placeholder="Enter your Password" />
+            <input value={password.value} onChange={password.changeHandler} className="border mt-2 outline-none p-2 rounded-md bg-transparent text-white" type="password" placeholder="Enter your Password" />
+          </div>
+          {
+            password.error && (
+              <Typography color="error" variant="caption">
+                {password.error}
+              </Typography>
+            )
+          }
+          
+          {/* <TextField
+          required
+          fullWidth
+          label= "Password"
+          type = "password"
+          margin="normal"
+          variant="outlined"
+          value={password.value}
+          onChange={password.changeHandler}
+           />
+          {
+            password.error && (
+              <Typography color="error" variant="caption">
+                {password.error}
+              </Typography>
+            )
+          } */}
+          <div className="flex justify-center bg-red-500 mx-auto rounded-lg">
+            <button className="text-white text-2xl px-3 py-1 rounded-lg">{islogin ? "Login" : "SignUp"}</button>
           </div>
 
           <div className="text-white text-center text-xl">
