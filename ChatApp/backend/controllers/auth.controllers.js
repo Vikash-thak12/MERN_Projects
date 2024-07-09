2d53662a3cc0df2825f87f964d57142d829de980
Vikash Thakur <thakvikash622@gmail.com>
2024-07-09 16:49:58 +0530

    Handle the functionlaity of signup

--- ChatApp/backend/controllers/auth.controllers.js
+++ ChatApp/backend/controllers/auth.controllers.js
@@ -1,8 +1,50 @@
-export const signup = (req, res) => {
+import userModel from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
+export const signup = async (req, res) => {
     try {
-        const {fullname, gmail, password} = req.body;
+        const {fullname, gmail, password, confirmpassword, gender, profilepic} = req.body;
+        if(password !== confirmpassword) {
+            res.status(400).json({ Error: "The passwords didn't match"})
+        }
+
+        if(!fullname || !gmail || !password || !gender || !profilepic) {
+            res.status(401).json({
+                Error: "Please fill all the fields"
+            })
+        }
+
+        const user = await userModel.findOne({gmail})
+        if(user) {
+            res.status(401).json({ Error: "User already exists" })
+        }
+
+        const boyprofile = `https://avatar.iran.liara.run/public/boy?username={gmail}`;
+        const girlprofile = `https://avatar.iran.liara.run/public/girl?username={gmail}`
+
+
+        const hashpassword = await bcryptjs.hash(password, 8) 
+        await userModel.create({
+            fullname,
+            gmail,
+            password: hashpassword,
+            gender,
+            profilepic: gender === "male" ? boyprofile : girlprofile
+        })
+
+        res.status(200).json({
+            message: "User created successfully"
+        }, {
+            _id: user._id,
+            fullname,
+            gmail,
+            gender
+        })
+
     } catch (error) {
-        console.log(Error);
+        console.log("Error in signup Controller", error.message);
+        res.status(500).json({
+            Error: "Internal Server Error"
+        })
     }
 }
 
