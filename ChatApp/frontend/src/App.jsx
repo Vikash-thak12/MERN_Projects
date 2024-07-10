f62efb25130aa38b2b551cfc6277f30a870ae832
Vikash Thakur <thakvikash622@gmail.com>
2024-07-10 20:44:52 +0530

    Install daisy theme

--- ChatApp/frontend/src/App.jsx
+++ ChatApp/frontend/src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
   return (
     <div>
-    App
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
     </div>
   );
 };
