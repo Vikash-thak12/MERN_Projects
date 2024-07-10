7be4d9a3601abab5cc3829a2b9323fd347b2405c
Vikash Thakur <thakvikash622@gmail.com>
2024-07-10 23:29:20 +0530

    Just update the code

--- ChatApp/frontend/src/pages/Login.jsx
+++ ChatApp/frontend/src/pages/Login.jsx
@@ -2,8 +2,8 @@
 
 const Login = () => {
     return (
-        <div className="w-full flex items-center justify-center">
-            <div className="border rounded-lg w-1/4 px-5 py-4">
+        <div className="w-full flex items-center justify-center bg-blur">
+            <div className="border rounded-lg w-1/4 px-5 py-4 bg-gray-700 backdrop-filter backdrop-blur-lg">
                 <h1 className="text-center text-3xl">Login <span className="text-blue-600 cursor-pointer">ChatApp</span></h1>
                 <form className="flex flex-col gap-4">
                     <div className="flex flex-col gap-2">
@@ -15,7 +15,7 @@ const Login = () => {
                         <input className="px-3 py-2 outline-none rounded-md" type="password" name="password" placeholder="Enter your Password" />
                     </div>
 
-                <button className="bg-blue-500 mt-3 rounded-md cursor-pointer text-white px-4 py-1">Login</button>
+                <button className="bg-blue-500 font-bold mt-3 rounded-md cursor-pointer text-white px-4 py-1">Login</button>
                 </form>
                 <h1 className="mt-4 text-center">{"Didn't"} Have an Account. <span className="text-blue-500 cursor-pointer">SignUp</span></h1>
             </div>
