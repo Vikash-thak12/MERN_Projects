437d6dd4409310ecfe002170d75650ae611fb016
Vikash Thakur <thakvikash622@gmail.com>
2024-07-12 15:08:44 +0530

    Protected the route with authcontext

--- ChatApp/frontend/src/App.jsx
+++ ChatApp/frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home/Home";
 import Signup from "./pages/Signup";
 import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from "./context/AuthContext";
 
 
 const App = () => {
+  const { authUser } = useAuthContext()
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
       </Routes>
       <Toaster />
     </div>
