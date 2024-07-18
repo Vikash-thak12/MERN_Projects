2f42d7d3af9789a5b1f72e9145feb63c4919ce15
Vikash Thakur <thakvikash622@gmail.com>
2024-07-18 14:02:52 +0530

    Initial commit

--- BloggingApp/index.js
+++ BloggingApp/index.js
@@ -1,1 +1,14 @@
-console.log("hey");
+import express from "express";
+import path from "path"
+
+const app = express();
+const PORT = 8000
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
+app.get("/", (req,res) => {
+    res.render("home")
+})
+
+app.listen(PORT, () => console.log(`Server started at Port: ${PORT}`))
